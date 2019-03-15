ab6414ea61a312922ef6f581c1f4200f754523ae
Elise Phan <aliceconnic@gmail.com>
2019-03-15 00:42:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ class BoardMember {
   approve(){
     return 'You can do that!'
   }
+  
   doCharity(){
     return "I like to help people."
   }
 
-  BoardMember.prototype.releasePressStatement = function(){
+  releasePressStatement(){
     return "You will see great things from Scuber."
   }
 
-  BoardMember.prototype.sayHi = function(){
+  sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
