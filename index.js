86b5270f2ffd2cdd34d632c20701334ab11296f2
Elise Phan <aliceconnic@gmail.com>
2019-03-15 00:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ class BoardMember {
   this.homeState = homeState
   this.training = training
 
-
   BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
   }
@@ -22,3 +21,4 @@ class BoardMember {
   BoardMember.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
+}
